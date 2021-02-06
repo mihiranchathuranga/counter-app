7f2b7150294fddf88274d1e9dd0b79270e01614c
mihiran <154015E@uom.lk>
2021-02-06 13:18:11 +0530

    did changes in Button component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 //give Counter as component name
 class  Counter extends Component {
+
+    //define the state which contains with required data for component
+    state = {
+        count: 0,
+       };
+    
     
     render() { 
-        return (<h1>Hello World</h1>  );
-    }
-}
+        return (
+            <div>
+             <span>Hello World</span>
+             <button>Increment</button>
+            </div>
+        )
+                }
+        }
  
 //exporting the Counter
 export default Counter ;
