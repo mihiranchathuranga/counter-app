a3eedf41f5e1eb349c57d18a4ea75c8b87f7e305
mihiran <154015E@uom.lk>
2021-02-06 15:13:46 +0530

    add boostrap to button class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,8 +15,8 @@ class  Counter extends Component {
         return (
             <div>
               
-             <span className="badge badge-primary">{this.formatCount()}</span>
-             <button>Increment</button>
+             <span className="badge badge-primary m-2">{this.formatCount()}</span>
+             <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         )
                 }
