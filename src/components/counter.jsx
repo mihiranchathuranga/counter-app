12dc6c8fe0b42c7d60608ae6680508a99d078aa6
mihiran <154015E@uom.lk>
2021-02-06 16:27:12 +0530

    add inline css

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,18 +9,15 @@ class  Counter extends Component {
         count: 0,
        
        };
-
-    styles = {
-        fontSize : 10,
-        fontWeight: "bold"
-    };
+    //we can use this way to apply styles
+   
     
     //how to add img to div
     render() { 
         return (
             <div>
               
-             <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+             <span style={{fontSize:30}} className="badge badge-primary m-2">{this.formatCount()}</span>
              <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         )
