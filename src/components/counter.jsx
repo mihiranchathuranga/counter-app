2d28796f1fd700f213d90b229d4f2b2eeb6d74be
mihiran <154015E@uom.lk>
2021-02-06 12:32:58 +0530

    create new component counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+//give Counter as component name
+class  Counter extends Component {
+    
+    render() { 
+        return (<h1>Hello World</h1>  );
+    }
+}
+ 
+//exporting the Counter
+export default Counter ;
