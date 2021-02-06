f98e5a5a8bd755b16cd0aa061bf96c6f31e8032f
mihiran <154015E@uom.lk>
2021-02-06 15:17:47 +0530

    add style attribute to span class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,13 +9,18 @@ class  Counter extends Component {
         count: 0,
        
        };
+
+    styles = {
+        fontSize : 10,
+        fontWeight: "bold"
+    };
     
     //how to add img to div
     render() { 
         return (
             <div>
               
-             <span className="badge badge-primary m-2">{this.formatCount()}</span>
+             <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
              <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         )
