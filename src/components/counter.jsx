60acfe8600037205e87c553504a2e566a5961cf4
mihiran <154015E@uom.lk>
2021-02-06 23:11:40 +0530

    temporarily removed helper methods

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,35 +7,29 @@ class  Counter extends Component {
     //define imageUrl
     state = {
         count: 0,
-        tags: ['tag1','tag2','tag3']       
+        tags: []       
        };
     renderTags(){
         if(this.state.tags.length === 0) return<p>There are no tags!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+
+    handleIncrement(){
+        console.log('Increment Clicked');
     }
     render() { 
        // let classes = this.getBadgeClasses();
         return (
-            <div>           
-             <ul>
-               {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-             </ul>
+            <div>
+             {/*conditional rendering second way */}
+             {this.state.tags.length === 0 && "Please create a new tag!"}           
+             {this.renderTags()}
             </div>
         )
                 }
 
  
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-formatCount(){
-    //object destrcturing
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
-    //return this.state.count === 0 ? 'Zero' : this.state.count;
-}
+    
 
 }
  
